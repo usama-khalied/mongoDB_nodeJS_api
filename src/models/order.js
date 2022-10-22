b8f601e005f222b4fd7c57a1473424bd9075633a
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-10-22 06:38:39 -0700

    #Structure#Implement

---
+++ src/models/order.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+
+
+const Order = {
+    name:String,
+    email:String,
+    phone:String,
+    address:String,
+    oid:Number,
+    price:Number,
+    qty:Number,
+    currentDate:String,
+}
+const OrderSchema = mongoose.model("NEWCOL",Order);
+module.exports = OrderSchema
