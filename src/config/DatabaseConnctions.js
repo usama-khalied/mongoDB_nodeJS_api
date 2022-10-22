eab3d1a861c8076f10d66dab33fc9baf39191acb
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-10-22 06:38:24 -0700

    #Structure#Implement

---
+++ src/config/DatabaseConnctions.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require('dotenv').config()
+const DATABASE_URL = process.env.DATABASE_URL
+
+
+mongoose.connect(`${DATABASE_URL}/mynewdb`,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}, (err) => {
+     if(!err) {
+        console.log("Database is connected");
+     }
+     else {
+        console.log("Db is not connected")
+     }
+});
