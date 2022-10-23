d583660166ecbb74e4dc4c6e937db3e9008dded2
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-10-23 03:10:18 -0700

    Organized#Api

--- src/routes/order.js
+++ src/routes/order.js
@@ -4,7 +4,21 @@ const orderControllers  = require('../controllers/order')
 
 
 
+// Get All Orders using this route - complete testing ✔✔✔
+routes.get("/getAllOrdersData",orderControllers.getAllOrders);
 
-routes.get("/getAllProductsData",orderControllers.getAllOrders);
+// Post Method using this route - complete testing ✔✔✔
+routes.post("/postData",orderControllers.postOrder);
+
+// Update Method using this route - complete testing ✔✔✔
+routes.put("/updateOrder/:oid",orderControllers.updatOrder);
+
+// Get Order By Id using this route - complete testing ✔✔✔
+routes.get("/fetchOrder/:oid",orderControllers.getOrderById);
+
+// Get Order By OrderID using this route  - complet testing ✔✔✔
+routes.delete("/del/:oid",orderControllers.delByOrderId);
+
+
 
 module.exports = routes
